05fa49f5a9d405e0393ed55410add4cf0eb0eb15
mmumby <meredith.mumby@outlook.com>
2017-10-17 19:28:47 +0000

    rendered project data from backend server to dry up portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,7 +8,8 @@ class Portfolio extends Component {
     super();
 
     this.state = {
-        inMouseInside: false
+        inMouseInside: false,
+        projects: []
     };
   }
 
@@ -24,58 +25,42 @@ class Portfolio extends Component {
     });
   }
 
+  componentDidMount() {
+    console.log("sending request...")
+    fetch('/users')
+      .then(res => res.json())
+      .then(projects => this.setState({ projects }));
+  }
+
   render() {
     return (
       <div id="my-portfolio" className="portfolio">
-        <div className="portfolio-left">
-          <ul>
-            <li onMouseEnter={this.mouseEnter.bind(this)} onMouseLeave={this.mouseExit.bind(this)}>
-              {this.state.isMouseInside ? <button className="viewmore-portfolio">View More</button> : null}
-              <img className="portfolio-images" src={require('../styles/img/jobSearch.png')} alt="Frame"/>
-            </li>
-            <li>
-              <img className="portfolio-images" src={require('../styles/img/lettuceShare.png')} alt="Frame"/>
-            </li>
-            <li>
-              <img className="portfolio-images" src={require('../styles/img/ChattyApp.png')} alt="Frame"/>
-            </li>
-            <li>
-              <img className="portfolio-images" src={require('../styles/img/WikiMaps.png')} alt="Frame"/>
-            </li>
-            <li>
-              <img className="portfolio-images" src={require('../styles/img/tweeter-photo.png')} alt="Frame"/>
-            </li>
-          </ul>
+      {this.state.projects.map(project =>
+        <div key={project.display_order}>
+          <div className="portfolio-left">
+            <ul>
+              <li onMouseEnter={this.mouseEnter.bind(this)} onMouseLeave={this.mouseExit.bind(this)}>
+                {this.state.isMouseInside ? <button className="viewmore-portfolio">View More</button> : null}
+                <img className="portfolio-images" src={project.image_url}/>
+              </li>
+            </ul>
+          </div>
+          <div className="portfolio-right">
+            <ul>
+              <li>
+                <h1>{project.project_name}</h1>
+                {project.tech_stack.map((stack, index) =>
+                  <div key={index}>
+                    <p> {stack} | </p>
+                  </div>
+                  )}
+                <p>{project.description}</p>
+                <a href={project.github_url}><i class="fa fa-2x fa-github" aria-hidden="true"></i>view source code</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="portfolio-right">
-          <ul>
-            <li>
-              <h1>JobSearch</h1>
-              <p>React | Redux</p>
-              <a href="https://github.com/mmumby/jobSearch"><i class="fa fa-2x fa-github" aria-hidden="true"></i>view source code </a>
-            </li>
-            <li>
-              <h1>lettuceShare</h1>
-              <p>Ruby |  Rails | WebSockets | PostgreSQL | Bootstrap | SASS</p>
-              <a href="https://github.com/mmumby/Lettuce-Share"><i class="fa fa-2x fa-github" aria-hidden="true"></i>view source code </a>
-            </li>
-            <li>
-              <h1>ChattyApp</h1>
-              <p>Express | Node | React | WebSockets</p>
-              <a href="https://github.com/mmumby/chattyApp"><i class="fa fa-2x fa-github" aria-hidden="true"></i>view source code </a>
-            </li>
-            <li>
-              <h1>WikiMaps</h1>
-              <p>Bootstrap | Express | jQuery | Node | PostgreSQL | SASS</p>
-              <a href="https://github.com/mmumby/WikiMap"><i class="fa fa-2x fa-github" aria-hidden="true"></i>view source code </a>
-            </li>
-            <li>
-              <h1>Tweetr</h1>
-              <p>Express | jQuery | MongoDB | Node </p>
-              <a href="https://github.com/mmumby/tweetr"><i class="fa fa-2x fa-github" aria-hidden="true"></i>view source code </a>
-            </li>
-          </ul>
-        </div>
+        )}
       </div>
     );
   }
